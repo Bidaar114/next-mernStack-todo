dfd6a16b81f77468a14ab12e5fa4b01cee446ef5
Ibrahim-Hussein <ihusein114@gmail.com>
2024-01-14 04:18:15 -0800

    vercel

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,4 @@
 
 
-export const API_URL = process.env.NODE_URL === 'production'? 'https://next-mern-stack-todo-9gmk-qkskgib9s-bidaar114.vercel.app/': 'http://localhost:3000'
+export const API_URL = process.env.NODE_URL === 'production' ? 'https://next-mern-stack-todo-9gmk-qkskgib9s-bidaar114.vercel.app/'
+: 'http://localhost:3000'
